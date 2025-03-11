72f5b929c16510685727805901da6e9da8d84581
Lixel Amos <48270069+lixelamos@users.noreply.github.com>
2025-03-11 14:16:25 +0300

    Update projects.vue

--- amosp/src/components/projects.vue
+++ amosp/src/components/projects.vue
@@ -133,12 +133,12 @@ residential and commercial properties.</p>
                     <div class="project-info">
 
                            <div class="project-section-icons-container">
-                            <a  target="_blank" class="code" href="https://github.com/lixelamos/phase-1-tasklister-mini-project">
+                            <a  target="_blank" class="code" href="https://github.com/lixelamos/prescripto">
                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg> 
                             </a>
 
                             <div class="github-and-external-link">
-                                <a  target="_blank" href="https://my-to-do-task.netlify.app/">
+                                <a  target="_blank" href="https://prescripto-gfwx-eant4r86x-lixelamos-projects.vercel.app/">
                                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
                                     </svg>
                                 </a>
@@ -146,10 +146,16 @@ residential and commercial properties.</p>
                         </div>   
 
                      <h1>my-to-do-task</h1>
-                     <p>A to-do app is a software application designed to help individuals manage and organize their tasks and responsibilities. It provides a digital platform where users can create, track, and prioritize their to-do lists.</p>
+                     <p>A fully functional doctor appointment booking platform that allows patients to book appointments, make online payments, and manage their bookings. The system includes an admin panel for managing doctors, appointments, and payments. Built with a modern Full Stack architecture, the platform ensures seamless performance, security, and user experience.</p>
                      <div class="tech-used-container">
-                         <p class="tech-used">HTML</p>
-                         <p class="tech-used">Javascript</p>
+                         <p class="tech-used">TypeScript</p>
+                         <p class="tech-used">Tailwind CSSt</p>
+                                                  <p class="tech-used">MONGO DB</p>
+
+                                                  <p class="tech-used">REACT JS</p>
+
+                                                  <p class="tech-used">Node JS</p>
+
                      </div>
                     </div>
                 </div>
