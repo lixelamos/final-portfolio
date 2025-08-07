894ad1db1a389979d13c04485953102ebe2dc423
Lixel Amos <48270069+lixelamos@users.noreply.github.com>
2025-08-07 09:33:21 +0300

    Update projects.vue

--- amosp/src/components/projects.vue
+++ amosp/src/components/projects.vue
@@ -62,27 +62,27 @@
                     
                     <div class="project-info">
                         <div class="project-section-icons-container">
-                            <a  target="_blank" class="code" href="https://github.com/lixelamos/Recipe-app">
+                            <a  target="_blank" class="code" href="https://github.com/lixelamos/QuickAi">
                                <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                             </svg> 
                             </a>
 
                             <div class="github-and-external-link">
-                                <a  target="_blank" href="https://recipe-app-7a38f4.netlify.app">
+                                <a  target="_blank" href="https://quick-ai-flax-two.vercel.app/">
                                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><title>External Link</title><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
                                     </svg>
                                 </a>
                             </div>
                         </div>
 
-                        <h1>Recipe app</h1>
-                        <p>I developed a simple recipe app as a learning project to work with APIs. I found a well-documented API that provides recipe data, and used it to build the app. The app allows users to search for recipes based on their preferences and interests</p>
+                        <h1>QuickAi</h1>
+                        <p>QuickAI, is a powerful, all-in-one AI SaaS platform built to boost your productivity and creativity</p>
                      
                         <div class="tech-used-container">
-                            <p class="tech-used">HTML</p>
-                            <p class="tech-used">Javascript</p>
-                            <p class="tech-used">CSS</p>
-                            <p class="tech-used">Fetch API</p>
+                            <p class="tech-used">React</p>
+                            <p class="tech-used">Typescript</p>
+                            <p class="tech-used">TailwindCSS</p>
+                            <p class="tech-used">Express</p>
                         </div>
     
                     </div>
@@ -117,7 +117,7 @@ residential and commercial properties.</p>
                             <p class="tech-used">CSS</p>
                             <p class="tech-used">Javascript</p>
                             <p class="tech-used">React.js</p>
-                            <p class="tech-used">python</p>
+                            <p class="tech-used">Flask</p>
 
                         </div>
 
